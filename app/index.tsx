34ec75b7448b5356047b1fe139fcaf766c70e837
pramodreddypandiri <pramodreddypandiri010@gmail.com>
2026-01-15 16:14:57 -0500

    fixed node issues

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
+import "./globals.css"
 import { Text, View } from "react-native";
-
-export default function Index() {
+ 
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Home Screen</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to My app!
+      </Text>
     </View>
   );
 }
